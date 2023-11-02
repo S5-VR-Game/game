6e18669c4537611f90c9f482719ca2e13a374c79
larnold <praisethesun@firelink-shrine.de>
2023-11-02 17:08:32 +0100

    cleanup

--- Assets/Scripts/UI/TextPanel.cs
+++ Assets/Scripts/UI/TextPanel.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.UI;
 
 public class TextPanel : MonoBehaviour
 {
