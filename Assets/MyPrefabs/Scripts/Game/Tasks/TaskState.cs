6df6f1a432da4bc02ee106180a4075f9fa949f3d
Julius Arendt <julius.arendt@klosebrothers.de>
2023-11-15 22:05:35 +0100

    add TaskState enum

---
+++ Assets/MyPrefabs/Scripts/Game/Tasks/TaskState.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace MyPrefabs.Scripts.Game.Tasks
+{
+    /**
+     * This enum represents the state of a specific Task
+     */
+    public enum TaskState
+    {
+        SUCCESSFUL, ONGOING, FAILED
+    }
+}
