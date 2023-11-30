9e5a32a5be9d3b02574654047f78b53e80a35bf9
JonasPTFL <js.pp@gmx.de>
2023-11-30 16:54:18 +0100

    first approach on structure for implementing the mixing ingredients task - adds factory and task class - adds first prefabs - adds test materials

---
+++ Assets/Scripts/Game/Tasks/MixingIngredients/MixingIngredients.cs
@@ -0,0 +1,33 @@
+﻿namespace Game.Tasks.MixingIngredients
+{
+    public class MixingIngredients : GameTask
+    {
+        private const string Name = "Mixing ingredients";
+        private const string Description = "Task decsription";
+        
+        public MixingIngredients() : base(Name, Description)
+        {
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        protected override void BeforeStateCheck()
+        {
+        }
+
+        protected override TaskState CheckTaskState()
+        {
+            return TaskState.Ongoing;
+        }
+
+        protected override void AfterStateCheck()
+        {
+            if (currentTaskState != TaskState.Ongoing)
+            {
+                DestroyTask();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Tasks/MixingIngredients/MixingIngredientsFactory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Game.Tasks.MixingIngredients
+{
+    public class MixingIngredientsFactory : GameTaskFactory<TaskSpawnPoint>
+    {
+        [SerializeField] private MixingIngredients mixingIngredientsPrefab;
+        
+        protected override GameTask CreateTask(TaskSpawnPoint spawnPoint)
+        {
+            GameObject instance = Instantiate(mixingIngredientsPrefab.gameObject, spawnPoint.GetSpawnPosition(), Quaternion.identity);
+            MixingIngredients exampleTimerGameTask = instance.GetComponent<MixingIngredients>();
+
+            return exampleTimerGameTask;
+        }
+    }
+}
