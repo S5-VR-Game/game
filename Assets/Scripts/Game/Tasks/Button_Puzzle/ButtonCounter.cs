0f9e78081a20017cf38e26cd528829504cee08d6
Jarasch <Jockel.blooba@gmx.de>
2023-12-02 17:14:38 +0100

    Added PaintingTests

--- Assets/Scripts/Game/Tasks/Button_Puzzle/ButtonCounter.cs
+++ Assets/Scripts/Game/Tasks/Button_Puzzle/ButtonCounter.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Prefabs.Game.Tasks.Button_testing;
+using TMPro;
 using UnityEngine;
 
 public class ButtonCounter : MonoBehaviour
 {
+    [SerializeField] private TextMeshPro textField;
+
+    [SerializeField] private ButtonSequenceLogic logic;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +18,28 @@ public class ButtonCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Test(0);
+        } 
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            Test(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            Test(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            Test(3);
+        }  
     }
 
-    public void Test()
+    public void Test(int color)
     {
-        print("button pressed");
+        textField.text = color.ToString();
+        print(color.ToString());
+        logic.ButtonCheck((ColorCode)color);
     }
 }
--- Assets/Scripts/Game/Tasks/Button_Puzzle/ButtonSequenceLogic.cs
+++ Assets/Scripts/Game/Tasks/Button_Puzzle/ButtonSequenceLogic.cs
@@ -1,26 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using Game.Tasks;
+using Prefabs.Game.Tasks.Button_testing;
 using UnityEngine;
 
-public class ButtonSequenceLogic : MonoBehaviour
+
+public class ButtonSequenceLogic : GameTask
 {
-    private int[] buttonOrder;
+    private int[] colorSequence;
+    private bool done = false;
+    private int level = 0;
+    const int sequenceLenght = 9;
     
-    // Start is called before the first frame update
-    void Start()
+    public override void Initialize()
+    {
+        
+        //create Sequence
+        for (int i = 0; i < sequenceLenght; i++)
+        {
+            colorSequence[i] = Random.Range(0, 4);
+            print(colorSequence[i]);
+        }
+    }
+
+    protected override void BeforeStateCheck()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override TaskState CheckTaskState()
     {
-        
+        if (done)
+        {
+            return TaskState.Successful;
+        }
+        else
+        {
+            return TaskState.Ongoing;
+        }
     }
 
-    public void ButtonPressed()
+    protected override void AfterStateCheck()
     {
         
     }
     
+    //compares the pressed button with the next Color in the Sequence
+    public void ButtonCheck(ColorCode color)
+    {
+        if (color == (ColorCode) colorSequence[level])
+        {
+            if (level == sequenceLenght)
+            {
+                done = true;
+                print("done");
+            }
+            level =+ 1;
+        }
+        else
+        {
+            print("reset");
+            level = 0;
+        }
+    }
+
+    public ButtonSequenceLogic() : base("ButtonSequence", "description")
+    {
+    }
 }
