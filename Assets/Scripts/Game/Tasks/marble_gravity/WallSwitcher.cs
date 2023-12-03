781fe65f15f2f4302135ed0b97269faaf5ba1df0
Julius Arendt <julius.arendt@klosebrothers.de>
2023-12-03 18:07:38 +0100

    update WallSwitcher.cs:  - Will now destroy the switch and the wall

--- Assets/Scripts/Game/Tasks/marble_gravity/WallSwitcher.cs
+++ Assets/Scripts/Game/Tasks/marble_gravity/WallSwitcher.cs
@@ -1,16 +1,22 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Game.Tasks.marble_gravity
 {
     /// <summary>
-    /// This Script just turns on and off the
-    /// walls in the difficult task
+    /// This Script destroys the wall
+    /// and the switch if the Sphere is entering
+    /// the given switch.
+    ///
+    /// NOTE: this script can only be assigned to switches.
     /// </summary>
     public class WallSwitcher : MonoBehaviour
     {
+        private const string Name = "sphere";
+        public GameObject wall;
         private void OnCollisionExit(Collision other)
         {
+            if (!other.gameObject.name.Equals(Name)) return;
+            Destroy(wall);
             Destroy(gameObject);
         }
     }
