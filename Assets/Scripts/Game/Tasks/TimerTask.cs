2826016b932afc7bdbc9c9857929000800be435e
Julius Arendt <julius.arendt@klosebrothers.de>
2023-12-04 15:16:08 +0100

    add feature of timer start condition

--- Assets/Scripts/Game/Tasks/TimerTask.cs
+++ Assets/Scripts/Game/Tasks/TimerTask.cs
@@ -5,6 +5,7 @@ namespace Game.Tasks
     public abstract class TimerTask : GameTask
     {
         protected float remainingTime;
+        [SerializeField] protected bool timerStarted;
 
         protected TimerTask(
             float initialTimerTime,
@@ -17,10 +18,28 @@ namespace Game.Tasks
         }
 
         /// <summary>
+        /// Determines to a certain condition whether the Timer
+        /// Task should start or not
+        ///
+        /// It can deliver true by default, or be implemented by
+        /// a subclass.
+        /// </summary>
+        /// <returns>true, if that's the case, false otherwise</returns>
+        protected bool TimerStartCondition()
+        {
+            return true;
+        }
+
+        /// <summary>
         /// Overriden update method for time limited tasks.
         /// </summary>
         protected sealed override void Update()
         {
+            if (!timerStarted)
+            {
+                timerStarted = TimerStartCondition();
+                return;
+            }
             if (remainingTime < 0 && currentTaskState == TaskState.Ongoing)
             {                    
                 // IMPORTANT NOTE: If the time is up, there needs to be Failed in Future!!!
