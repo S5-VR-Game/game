3a535b2c4564f2082e978d2e03f6a34643690647
JonasPTFL <js.pp@gmx.de>
2023-12-05 17:21:31 +0100

    add xr knob to valve - needs to be tested, if the screw mechanic can be implemented using the xr know script

--- Assets/Scripts/Game/Tasks/MedicalDisaster/Valve.cs
+++ Assets/Scripts/Game/Tasks/MedicalDisaster/Valve.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VRTemplate;
 using UnityEngine;
 
 namespace Game.Tasks.MedicalDisaster
@@ -11,58 +12,26 @@ namespace Game.Tasks.MedicalDisaster
     public class Valve : MonoBehaviour
     {
         [SerializeField] private MeshRenderer valveMeshRenderer;
-        [SerializeField] private Transform valveTransform;
+        [SerializeField] private XRKnob valve;
         public int requiredRotationCount = 3;
         public event Action OnValveRotationCompleted;
 
-        private bool m_HalfRotation;
         private bool m_ValveRotationCompleted;
         private int m_RotationCount;
-        private Vector3 m_OldForward;
 
         private readonly Color m_ClosedColor = Color.red;
         private readonly Color m_OpenedColor = Color.green;
-        private const float RotationThreshold = 0.7f;
 
         private void Start()
         {
             valveMeshRenderer.material.color = m_ClosedColor;
-            m_OldForward = valveTransform.forward;
         }
 
-        private void Update()
+        public void OnRotationChanged()
         {
-            var forward = valveTransform.forward;
-            Vector3 cross = Vector3.Cross(m_OldForward, forward);
-            m_OldForward = forward;
-
-            // check if half rotation is completed
-            if (!m_HalfRotation && valveTransform.rotation.z <= -RotationThreshold && cross.z >= 0)
-            {
-                m_HalfRotation = true;
-            }
-            else if (m_HalfRotation && valveTransform.rotation.z >= 0)
-            {
-                // full rotation completed
-                m_HalfRotation = false;
-                m_RotationCount++;
-                if (!m_ValveRotationCompleted && m_RotationCount >= requiredRotationCount)
-                {
-                    // valve rotation goal reached
-                    m_ValveRotationCompleted = true;
-                    OnValveRotationCompleted?.Invoke();
-                    valveMeshRenderer.material.color = m_OpenedColor;
-                }
-            }
+            // TODO test if screw mechanic can be implemented using xr knob
+            print(valve.value);
+            valveMeshRenderer.material.color = m_OpenedColor;
         }
-        
-        // private void LateUpdate()
-        // {
-        //     var currentTransform = valveTransform;
-        //     currentTransform.position = m_LastTransform.position;
-        //     var lastRotation = m_LastTransform.rotation;
-        //     valveTransform.rotation.Set(currentTransform.rotation.x, lastRotation.y, lastRotation.z, lastRotation.w);
-        //     m_LastTransform = currentTransform;
-        // }
     }
 }
