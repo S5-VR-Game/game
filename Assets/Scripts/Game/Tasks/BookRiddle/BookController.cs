42e2ce06df78b3f921195169860ae46f3351cef5
Julius Arendt <julius.arendt@klosebrothers.de>
2023-12-07 14:40:19 +0100

    add book prefab text

--- Assets/Scripts/Game/Tasks/BookRiddle/BookController.cs
+++ Assets/Scripts/Game/Tasks/BookRiddle/BookController.cs
@@ -1,9 +1,14 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 namespace Game.Tasks.BookRiddle
 {
     public class BookController : MonoBehaviour
     {
-        
+        private void Update()
+        {
+            // Debug.Log(GetComponentInChildren<TextMeshPro>().text);
+        }
     }
 }
