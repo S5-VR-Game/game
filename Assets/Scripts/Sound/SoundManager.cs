a1ba038d90b9b2d82794066907d52b2dd16fcc46
MarianK99 <Mariankogeikg@gmx.de>
2023-12-11 21:11:03 +0100

    refactoring and adding comments

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -3,25 +3,37 @@ using UnityEngine;
 
 namespace Sound
 {
+    // class used to play sounds depending on the chosen value of playSoundTrigger
     public class SoundManager : MonoBehaviour
     {
         public AudioClip audioClip;  // stores the played audio file
-        private AudioSource _audioSource;
+        private AudioSource _audioSource; // stores the audio-source (needed to play the sound)
 
+        // enum to decide when the sound should be played
         public enum PlaySoundTrigger
         {
-            FunctionCall,
-            Collision,
-            PlayerMovement,
-            PlayerDistance,
-            Permanent
+            FunctionCall, // plays sound after calling PlaySound() in another script
+            Collision, // plays sound after the game-object collides with something
+            PlayerMovement, // plays sound when the player is moving
+            PlayerDistance, // plays sound when the player is a given range to the game-object
+            Permanent // plays sound permanently
         }
 
-        public PlaySoundTrigger playSoundTrigger;
-        private bool _isPlaying;
+        public PlaySoundTrigger playSoundTrigger; // value how the sound should be played
         
+        private bool _isPlaying; // value to stop the sound for the enum PlayerMovement
+        
+        
+        [Tooltip("You only need to set a value here if you are using the Play Sound Trigger 'Player Distance'.")]
+        public PlayerProfileService playerProfileService; // stores the reference to the player
+        
+        [Tooltip("You only need to set a value here if you are using the Play Sound Trigger 'Player Distance'.")]
+        public float playerDistance; // threshold value for the distance between the game-object and the player
+                                     // real distance > threshold -> no sound
+                                     // real distance <= threshold -> sound
         private void Start()
         {
+            // adds the audio-source to all game-objects who has this script
             _audioSource = gameObject.AddComponent<AudioSource>();
             if (audioClip != null)
             {
@@ -33,12 +45,11 @@ namespace Sound
                 Debug.LogError("No Sound found!");
             }
         }
-        
-        public PlayerProfileService playerProfileService;
-        public float playerDistance;
 
         private void Update()
         {
+            // PlaySoundTrigger = PlayerMovement
+            // plays sound if the player moves
             if (playSoundTrigger.Equals(PlaySoundTrigger.PlayerMovement))
             {
                 var movement = Input.GetAxis("Vertical") + Input.GetAxis("Horizontal");
@@ -59,6 +70,8 @@ namespace Sound
                 }
             }
 
+            // PlaySoundTrigger = PlayerDistance
+            // plays sound if the player has a specific distance to the game-object
             if (playSoundTrigger.Equals(PlaySoundTrigger.PlayerDistance))
             {
                 if (Vector3.Distance(gameObject.transform.position,
@@ -78,6 +91,8 @@ namespace Sound
                 }
             }
 
+            // PlaySoundTrigger = Permanent
+            // plays sound permanently
             if (playSoundTrigger.Equals(PlaySoundTrigger.Permanent))
             {
                 if (!_audioSource.isPlaying)
@@ -90,6 +105,8 @@ namespace Sound
 
         private void OnCollisionEnter(Collision other)
         {
+            // PlaySoundTrigger = Permanent
+            // plays sound on collision
             if (playSoundTrigger.Equals(PlaySoundTrigger.Collision))
             {
                 _audioSource.Play();
@@ -98,6 +115,8 @@ namespace Sound
         
         public void PlaySound()
         {
+            // PlaySoundTrigger = Permanent
+            // plays sound with function call
             if (!playSoundTrigger.Equals(PlaySoundTrigger.FunctionCall)) return;
             _audioSource.Play();
         }
