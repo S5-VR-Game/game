40372b828ac5aea2bffde6c92e172ac075eef1e6
MarianK99 <Mariankogeikg@gmx.de>
2023-12-12 01:19:39 +0100

    first try of removing start- and endscene camera from timeline

--- Assets/Scripts/Timeline/PlayStartScene.cs
+++ Assets/Scripts/Timeline/PlayStartScene.cs
@@ -1,3 +1,4 @@
+using PlayerController;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -7,23 +8,43 @@ namespace Timeline
     {
         // PlayableDirector is needed for the timeline to run
         public PlayableDirector timelineDirector;
-        
-        // store the sceme
-        public GameObject scene;
+
+        public PlayerProfileService playerProfileService;
+
+        public Transform startScenePlayerPosition;
 
         // Start is called before the first frame update
         private void Start()
         {
+            playerProfileService.SetVRMovementActive(false);
+            
+            playerProfileService.GetPlayerGameObject().transform.position = startScenePlayerPosition.position;
+            
+            playerProfileService.GetHUD().gameObject.SetActive(false);
+            
             // starts the start-scene
             timelineDirector.Play();
             // calls the function when the timeline is over
             timelineDirector.stopped += OnTimelineStopped;
         }
 
+        private void Update()
+        {
+            if (!playerProfileService.GetIsVrPlayerActive())
+            {
+                playerProfileService.GetPlayerGameObject().transform.position = startScenePlayerPosition.position;
+            }
+        }
+
         // is called when the timeline is over
         private void OnTimelineStopped(PlayableDirector director)
         {
-            scene.SetActive(false);
+            playerProfileService.SetVRMovementActive(true);
+            
+            playerProfileService.GetPlayerGameObject().transform.position = new Vector3(0, 2, 0);
+            playerProfileService.GetHUD().gameObject.SetActive(true);
+            
+            gameObject.SetActive(false);
         }
 
 
