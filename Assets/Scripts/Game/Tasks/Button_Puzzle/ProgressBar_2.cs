8dc6014c78fced78291a9e1e9c6d3bf429790f9f
Lukas Arnold <praisethesun@firelink-shrine.de>
2023-12-12 11:40:41 +0100

    functional 3d progrss shader

--- Assets/Scripts/Game/Tasks/Button_Puzzle/ProgressBar_2.cs
+++ Assets/Scripts/Game/Tasks/Button_Puzzle/ProgressBar_2.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 //https://devsplorer.wordpress.com/2020/05/18/creating-a-3d-progress-bar-with-shader-graph-devlog-tutorial/
 public class ShaderGraphProgressBar : MonoBehaviour
 {
-    float _FillRateValue = 0.51f; //progress bar starts empty
+    [Range(-0.51f, 0.51f)]
+    public float _FillRate = 0.51f; //progress bar starts empty
     Material objectMaterial;
 
     float stepSize = 0.1f; //progress is done by this value
@@ -12,9 +13,9 @@ public class ShaderGraphProgressBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        objectMaterial = new Material(Shader.Find("Shader Graphs/ProgressBarSingleAxis")); //creating a material with the shader
+        objectMaterial = new Material(Shader.Find("Shader Graphs/testgraph")); //creating a material with the shader
         gameObject.GetComponent<Renderer>().material = objectMaterial; //new material is applied to the game object
-        objectMaterial.SetFloat("_FillRate", _FillRateValue); //initial value is set 
+        objectMaterial.SetFloat("_FillRate", _FillRate); //initial value is set 
     }
 
                                         
@@ -22,12 +23,12 @@ public class ShaderGraphProgressBar : MonoBehaviour
     {                                   //if increase param is true, the progress bar progresses otherwise it deprogresses
         if (increase)
         {
-            _FillRateValue += stepSize; //progress increased
+            _FillRate += stepSize; //progress increased
         }
         else
         {
-            _FillRateValue -= stepSize; //progress decreased
+            _FillRate -= stepSize; //progress decreased
         }
-        objectMaterial.SetFloat("_FillRate", _FillRateValue); //Update the value of the progress bar
+        objectMaterial.SetFloat("_FillRate", _FillRate); //Update the value of the progress bar
     }
 }
