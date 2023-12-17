4d97d1111c5c13133a7915b7ffa2e74f29c33eb2
Julius Arendt <julius.arendt@klosebrothers.de>
2023-12-17 22:51:09 +0100

    add more book riddle spawn points according to the difficulty.

--- Assets/Scripts/Game/Tasks/BookRiddle/BookRiddleFactory.cs
+++ Assets/Scripts/Game/Tasks/BookRiddle/BookRiddleFactory.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Game.Tasks.BookRiddle
 {
@@ -16,6 +18,10 @@ namespace Game.Tasks.BookRiddle
         [SerializeField] private Material[] allBookMaterials;
 
         private const int MaxDigitsAndColors = 4;
+        
+        private const int MaxBooksEasy = 8;
+        private const int MaxBooksMedium = 16;
+        private const int MaxBooksHard = 32;
 
         // ReSharper disable Unity.PerformanceAnalysis
         protected override GameTask CreateTask(TaskSpawnPoint spawnPoint)
@@ -23,10 +29,48 @@ namespace Game.Tasks.BookRiddle
             var transform1 = spawnPoint.transform;
             var riddle = Instantiate(bookRiddlePrefab, transform1.position, transform1.rotation);
             var bookRiddle = riddle.GetComponent<BookRiddle>();
-            SpawnBooks(bookSpawnPoints, riddle.transform, bookRiddle);
+            SpawnBooks(GetAmountOfSpawnPoints(bookSpawnPoints), riddle.transform, bookRiddle);
             return bookRiddle;
         }
 
+        /// <summary>
+        /// Calculates according to the difficulty all spawn-points needed in order to spawn books
+        /// depending on the difficulty of the task.
+        /// </summary>
+        /// <param name="spawnPoints">all spawn points that exist.</param>
+        /// <returns>a list with the first n spawn points needed.</returns>
+        private Transform[] GetAmountOfSpawnPoints(IReadOnlyList<Transform> spawnPoints)
+        {
+            var amountOfSpawnPoints = GetAmountDependingOnDifficulty();
+            var newAmountOfSpawnPoints = new Transform[amountOfSpawnPoints];
+            for (var i = 0; i < amountOfSpawnPoints; i++)
+            {
+                newAmountOfSpawnPoints[i] = spawnPoints[i];
+            }
+            return newAmountOfSpawnPoints;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private int GetAmountDependingOnDifficulty()
+        {
+            return mDifficulty.GetSeparatedDifficulty() switch
+            {
+                SeparatedDifficulty.Easy => MaxBooksEasy,
+                SeparatedDifficulty.Hard => MaxBooksHard,
+                SeparatedDifficulty.Medium => MaxBooksMedium,
+                _ => MaxBooksEasy
+            };
+        }
+
+        /// <summary>
+        /// Spawns all the books necessary and also gives them their color.
+        /// </summary>
+        /// <param name="spawnPoints">All Spawn Points of the books that can be allocated.</param>
+        /// <param name="parent">The parent transform so that everything can be cleaned up later on.</param>
+        /// <param name="bookRiddle">The Book riddle where the solution is submitted.</param>
         private void SpawnBooks(Transform[] spawnPoints, Transform parent, BookRiddle bookRiddle)
         {
             spawnPoints.Shuffle();
@@ -84,6 +128,10 @@ namespace Game.Tasks.BookRiddle
             }
         }
         
+        /// <summary>
+        /// Retrieves a random digit in the range from 1 - 10
+        /// </summary>
+        /// <returns>Just the digit.</returns>
         private static int GetRandomDigit()
         {
             return Random.Range(0, 10);
