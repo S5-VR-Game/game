679df6041a379e8c763150fb856d145ea76a252f
Julius Arendt <julius.arendt@klosebrothers.de>
2024-01-10 13:48:36 +0100

    make smooth scaling

--- Assets/Scripts/Tutorial/ScaleCube.cs
+++ Assets/Scripts/Tutorial/ScaleCube.cs
@@ -5,14 +5,12 @@ namespace Tutorial
 {
     public class ScaleObject : MonoBehaviour
     {
-        private const float ScalingSpeed = 30.0f;
+        private const float ScalingSpeed = 50.0f;
         
         // The scale factor you want to apply
         public Vector3 scaleFactor = new Vector3(1f, 1f, 1f) * ScalingSpeed * Time.deltaTime;
 
         // Minimum and maximum scale values
-        public Vector3 minScale = new Vector3(0.5f, 0.5f, 0.5f);
-        public Vector3 maxScale = new Vector3(5f, 5f, 5f);
         private const float ControlDistance = 4.0f;
         [SerializeField]private PlayerProfileService playerProfileService;
 
@@ -26,30 +24,19 @@ namespace Tutorial
             // Scale up when the "ScaleUp" button is pressed
             if (Input.GetKeyDown(KeyCode.JoystickButton0))
             {
-                ScaleCube(scaleFactor);
+                ScaleCube(ScalingSpeed);
             }
 
             // Scale down when the "ScaleDown" button is pressed
             if (Input.GetKeyDown(KeyCode.JoystickButton1))
             {
-                ScaleCube(-scaleFactor);
+                ScaleCube(-ScalingSpeed);
             }
         }
 
-        private void ScaleCube(Vector3 additionalScale)
+        private void ScaleCube(float scale)
         {
-            // Access the Transform component of the game object
-            var objectTransform = transform;
-
-            // Calculate the new scale by adding the additional scale
-            var newScale = objectTransform.localScale + additionalScale;
-
-            // Clamp the scale values to stay within the specified min and max values
-            newScale = Vector3.Max(minScale, newScale);
-            newScale = Vector3.Min(maxScale, newScale);
-
-            // Modify the localScale property to scale the object
-            objectTransform.localScale = newScale;
+            transform.localScale *= scale * Time.deltaTime;
         }
     }
 }
