3c1c26f281aaf2050ec8cd426d944166d336b79a
JonasPTFL <js.pp@gmx.de>
2024-01-15 20:44:24 +0100

    removes unused debug log statement

--- Assets/Scripts/Game/Tasks/GameTaskFactory.cs
+++ Assets/Scripts/Game/Tasks/GameTaskFactory.cs
@@ -104,7 +104,6 @@ namespace Game.Tasks
                 // only register task to HUD navigation bar if alternative marker is not active
                 if (!m_PlayerProfileService.IsAltMarkerActive())
                 {
-                    Debug.Log("IsAltMarkerActive true");
                     // send task to HUD
                     m_PlayerProfileService.GetHUD().registerNewTask(newTask, spawnPoint.GetSpawnPosition(), newTask.taskPriority);
                 }
